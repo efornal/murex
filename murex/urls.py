e7f5febd9eb8e90a9ad07c726f2ec6d88ff7c5f9
esteban <efornal@gmail.com>
2015-03-09 13:28:31 -0300

    index test page

--- murex/urls.py
+++ murex/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'murex.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^app/', include('app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
