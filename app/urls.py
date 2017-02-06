98790843f9a10f269e2eef3dc90158270e1fa9c5
esteban <efornal@gmail.com>
2017-02-06 13:15:55 -0300

    Fixes to remove warnings from django updates

--- app/urls.py
+++ app/urls.py
@@ -1,17 +1,16 @@
-from django.conf.urls import patterns, url
-
+from django.conf.urls import url
 from app import views
 
-urlpatterns = patterns('app.views',
-    url(r'^logout/$', 'logout_view', name='logout'),
-    url(r'^login/','login_view', name='login'),
-    url(r'^toners/por_modelos/','toners_por_modelos', name='toners_por_modelos'),
-    url(r'^toners/por_estados/','toners_por_estados', name='toners_por_estados'),
-    url(r'^toners/filtrar/','filtrar_listado', name='filtrar_listado'),
-    url(r'^toners/','toners', name='toners'),
-    url(r'^toner/(\d+)/$', 'toner', name='toner'),
-    url(r'^search/','search', name='search'),
-    url(r'^toner/(\d+)/detail/$', 'toner_detail', name='toner_detail'),
-    url(r'^$', 'index', name='index'),
-)
+urlpatterns = [
+    url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^login/',views.login_view, name='login'),
+    url(r'^toners/por_modelos/',views.toners_por_modelos, name='toners_por_modelos'),
+    url(r'^toners/por_estados/',views.toners_por_estados, name='toners_por_estados'),
+    url(r'^toners/filtrar/',views.filtrar_listado, name='filtrar_listado'),
+    url(r'^toners/',views.toners, name='toners'),
+    url(r'^toner/(\d+)/$', views.toner, name='toner'),
+    url(r'^search/',views.search, name='search'),
+    url(r'^toner/(\d+)/detail/$', views.toner_detail, name='toner_detail'),
+    url(r'^$', views.index, name='index'),
+]
 
--- murex/urls.py
+++ murex/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('app.urls')),
-)
+]
 
 urlpatterns += i18n_patterns(
     url(r'^', include('app.urls')),
